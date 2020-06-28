0e1b37071343be8a1a82990c841eec26b9b354ec
loak155 <loak155.0620@gmail.com>
2020-06-28 20:18:26 +0900

    無限スクロール機能

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,14 @@
 class RoomsController < ApplicationController
   def show
-    # 投稿一覧表示に利用
-    @messages = Message.includes(:user).order(:id)
+    # 投稿一覧表示に利用。最新の１００件のみ取得する。
+    @messages = Message.includes(:user).order(:id).last(100)
     # メッセージ投稿に利用
     @message = current_user.messages.build
+  end
+
+  def show_additionally
+    # 追加のメッセージ50件を取得する
+    last_id = params[:oldest_message_id].to_i - 1
+    @messages = Message.includes(:user).order(:id).where(id: 1..last_id).last(50)
   end
 end
